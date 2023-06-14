6f1244978d69c804eb18ffc0c84724cee4cabc8e
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-06-14 11:35:04 +0900

    메서드 하이딩 예제

--- ShadowingHidingPrac/Program.cs
+++ ShadowingHidingPrac/Program.cs
@@ -10,22 +10,30 @@ namespace ShadowingHidingPrac31
         class Parent
         {
             public int variable = 273;
+            public void Method()
+            {
+                Console.WriteLine("부모의 메서드");
+            }
         }
 
         class Child : Parent
         {
             public new string variable = "shadowing";
+            public new void Method()
+            {
+                Console.WriteLine("자식의 메서드");
+            }
         }
 
         public static int number = 10;
-        static void Main(string[] args)
-        {
-            int number = 20;
-            Console.WriteLine(number);
-            Child child = new Child();
+        Expand All
+	@@ -26,6 +34,9 @@ static void Main(string[] args)
             Console.WriteLine(child.variable);
             Parent p = child;
-            Console.WriteLine(p.variable);
+        Console.WriteLine(p.variable);
+
+            child.Method();
+            p.Method();
         }
-    }
+}
 }
