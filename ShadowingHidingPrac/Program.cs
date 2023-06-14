83d20cefa02c6d354e25e4c31a451a091e332e00
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-06-14 11:34:39 +0900

    변수에서의 하이딩 예제

--- ShadowingHidingPrac/Program.cs
+++ ShadowingHidingPrac/Program.cs
@@ -7,11 +7,25 @@ namespace ShadowingHidingPrac31
 {
     internal class Program
     {
+        class Parent
+        {
+            public int variable = 273;
+        }
+
+        class Child : Parent
+        {
+            public new string variable = "shadowing";
+        }
+
         public static int number = 10;
         static void Main(string[] args)
         {
             int number = 20;
             Console.WriteLine(number);
+            Child child = new Child();
+            Console.WriteLine(child.variable);
+            Parent p = child;
+            Console.WriteLine(p.variable);
         }
     }
 }
