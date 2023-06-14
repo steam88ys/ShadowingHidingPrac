42ff36eaa6c7bb682363aa1cb59ebace1314d1d5
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-06-14 11:34:14 +0900

    Shadowing 예제

--- ShadowingHidingPrac/Program.cs
+++ ShadowingHidingPrac/Program.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace ShadowingHidingPrac
+namespace ShadowingHidingPrac31
 {
-    class Program
+    internal class Program
     {
+        public static int number = 10;
         static void Main(string[] args)
         {
+            int number = 20;
+            Console.WriteLine(number);
         }
     }
 }
